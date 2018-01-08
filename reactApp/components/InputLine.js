c65f98ac8ba4a81f6598a068443e9b99e2c49c16
Sunny <ssshah0624@gmail.com>
2018-01-07 22:04:33 -0800

    inputting todos successfully

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -3,13 +3,30 @@ import React from 'react'
 class InputLine extends React.Component{
   constructor(props){
     super(props)
+
+    this.state = {
+      typedText:'',
+    }
+  }
+
+  handleTyping(event){
+    this.setState({
+      typedText: event.target.value,
+    });
+  }
+
+  handleSubmit(){
+    this.props.submit(this.state.typedText);
+    this.setState({
+      typedText:'',
+    })
   }
 
   render(){
     return(
       <div className="inputLine">
-          <input type='text' placeholder='task'/>
-          <input type='submit' value="Add todo" onClick={() => this.props.submit("test task")}/>
+          <input type='text' placeholder='task' onChange={(event) => this.handleTyping(event)} val={this.state.typedText}/>
+          <button type='submit' onClick={() => this.handleSubmit()}>Add todo</button>
       </div>
     )
   }
