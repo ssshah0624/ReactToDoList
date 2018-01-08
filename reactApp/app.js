18b7c12f18b5a3251616c4b8357d866ce901f227
Sunny <ssshah0624@gmail.com>
2018-01-07 18:07:38 -0800

    added todo list elements from global var

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+let dummyData = ["Pick up Maria money", "Text Jesse about SEO content writer", "Report back on tech interview"]
 
-ReactDOM.render(<p>Tiger lives!</p>,
+
+class Todo extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <li><button type="button">X</button>{this.props.task}</li>
+    )
+  }
+}
+
+
+class TodoList extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <ul>
+        {dummyData.map((task) => (<Todo task={task}/>))}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
